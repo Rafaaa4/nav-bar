7e2e62eefe7561e6027458e27029d5b476fc0ee7
Rafaa Selmi <83070519+Rafaaa4@users.noreply.github.com>
2024-03-06 20:25:56 +0100

    Add files via upload

---
+++ nav-bar/js/main.js
@@ -0,0 +1,7 @@
+let  docTitle =  document.title;
+window.addEventListener('blur',()  =>{
+    document.title  = "Come back 😣";
+})
+window.addEventListener('focus',()  =>{
+    document.title  = docTitle;
+})
