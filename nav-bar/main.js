807713f3e4436c3dc1639f9ddff2ce6036a2ba2f
Rafaa Selmi <83070519+Rafaaa4@users.noreply.github.com>
2024-03-10 00:21:53 +0100

    Add files via upload

---
+++ nav-bar/main.js
@@ -0,0 +1,41 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
+
+let  docTitle =  document.title;
+window.addEventListener('blur',()  =>{
+    document.title  = "Come back 😣";
+})
+window.addEventListener('focus',()  =>{
+    document.title  = docTitle;
+})
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+
+animate();
+
