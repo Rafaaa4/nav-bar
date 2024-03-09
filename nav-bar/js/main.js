bcdfa0412d8499b53e66e7c8b86dc10b2d6ba6f9
Rafaa Selmi <83070519+Rafaaa4@users.noreply.github.com>
2024-03-10 00:20:07 +0100

    Delete nav-bar/js directory

--- nav-bar/js/main.js
+++
@@ -1,7 +0,0 @@
-let  docTitle =  document.title;
-window.addEventListener('blur',()  =>{
-    document.title  = "Come back 😣";
-})
-window.addEventListener('focus',()  =>{
-    document.title  = docTitle;
-})
